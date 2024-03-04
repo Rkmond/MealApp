060deed77e07dbfa691161d50ba3c0f1eaf5ce0e
Rkmond <160998613+Rkmond@users.noreply.github.com>
2024-03-04 21:34:32 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -139,7 +139,7 @@ const favCardCont = document.querySelector('#fav-card-container')
 
 
 // combining
-if (window.location.pathname.includes('/') {
+if (window.location.pathname.includes('/')) {
     // Code specific to index.html
     console.log("inside index")
     if(localStorage.favourites && JSON.parse(localStorage.favourites).length>0){
